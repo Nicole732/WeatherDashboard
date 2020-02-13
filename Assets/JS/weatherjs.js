7b0573fa743ba0e877e18773ca1419e7f57109b2
Your Name Dorinetk <dketchiozo@gmail.com>
2020-02-13 18:19:30 -0500

    initial

---
+++ Assets/JS/weatherjs.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+
+    var APIKey = "68872cd0166b535d960a63dbd5cd369e";
+    var currentDate =  moment().format("dddd, MMMM Do YYYY");
+    var cityDivGrab = $("#citieslisting");
+
+    var cityName = "";
+
+    function cityWeather(cityName) {
+
+        console.log(cityName);
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+        console.log(queryURL);
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+            }).then(function (response) {
+
+            // cityDivGrab.append($('<div class="card">'));
+            // cityDivGrab.append($('<div class="card-body">'));
+            var card = $('<div class="card">');
+            var cardBody = $('<div class="card-body">');
+
+            $("#citieslisting").prepend(card);  //put as a parent in HTML
+            card.append(cardBody);
+
+            var farTemperature = (response.main.temp_min - 273.15) * 1.80 + 32;
+
+            // if (farTemperature >=  cloud?) then icon 1 else icon 2
+            var iconcode = response.weather[0].icon;
+            var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+           
+
+           cardBody.append($('<h5 class="card-title">' + response.name + " (" + currentDate + ") " + "<img src='" + iconurl + "'>" + "</h5>"));
+            
+            
+            cardBody.append($('<p class="card-text">' + "Temperature: " + farTemperature.toFixed(0)  + "</p>"));
+            cardBody.append($('<p class="card-text">' + "Humidity: " + response.main.humidity + "</p>"));
+            cardBody.append($('<p class="card-text">' + "Wind Speed: " + response.wind.speed + "</p>"));
+            
+
+            //displayUvIndex(response.coord.lat, response.coord.lon);
+        });
+
+        
+
+    }
+
+    // grab the city name on click of search button
+    
+    
+    // cityWeather(cityN)
+   
+    $("#btnclick").on("click", function(event) {
+
+        event.preventDefault();
+        
+        var cityN = $("#cityInput").val().trim(); 
+        // forces a city name to be entered
+        if (cityN === "") {
+            return
+        }
+        console.log(cityN);
+        cityWeather(cityN);
+
+    });
+
+});
